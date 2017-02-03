36dcacb753e0073a9ec1bc34df4342bd658a33e4
Pim Rutgers <pim.rutgers@gmail.com>
2017-02-03 16:26:34 +0100

    Added SW-worker back in and fixed mana-font

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  staticFileGlobs: [
-    'build/Images/*/**.jpg'
-  ],
   stripPrefix: 'build/',
-  runtimeCaching: []
+  staticFileGlobs: [
+    'build/*.html',
+    'build/manifest.json',
+    'build/static/**/!(*map*)',
+		'build/Images/**/*.jpg',
+		'build/fonts/*.*'
+  ],
+  dontCacheBustUrlsMatching: /\.\w{8}\./,
+  swFilePath: 'build/service-worker.js'
 };
